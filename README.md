91df4596278fc5fdc0a40e9f770507ab3164f8bf
root <root@jibsheet.net>
2021-03-31 11:13:12 -0400

    added updates for Amazon EC2 for fork. added protected ip support - use .nobanip file. added a special new script for sendmail.
    	modified:   fail2ban.pl
    	new file:   fail2ban_sm.pl

--- README.md
+++ README.md
@@ -24,3 +24,19 @@ Step 5. If your Linux box is under attack, perhaps one minute later you'll have 
 0. '/usr/bin/perl', '/sbin/iptables', '/usr/bin/tail' must be available by 'root'
 1. '/var/log/secure' log should be in compatible with following PAM line ==> 
    "PAM 2 more authentication failures; logname= uid=0 euid=0 tty=ssh ruser= rhost=43.229.53.60  user=root"
+
+# Tested
+
+Only on CentOS-6.
+
+# Changes in this fork
+
+Added script for sendmail - blocks port 25 and looks at /var/log/maillog
+
+Runs on debian/amazon ec2
+
+changed what we are looking for in sshd
+
+also changed DENY to REJECT and added port checks for 22 and 25 per script
+
+if you want to protect a special ip - put in file .nobanip
--- fail2ban_sm.pl
+++ fail2ban_sm.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
 # -------------------------------------------------------------------------------------------------------
-# 1. Ban abusing ip addresses detected by PAM in file '/var/log/secure file"
-#    Ex. "PAM 2 more authentication failures; logname= uid=0 euid=0 tty=ssh ruser= rhost=43.229.53.60  user=root"
+# 1. Ban abusing ip addresses detected by did not issue MAIL/EXPN/VRFY/ETRN in file '/var/log/maillog file"
 # 2. Check duplication by 'iptables -L' command
+# **** sendmail *** port 25
 # -------------------------------------------------------------------------------------------------------
 
 my @iptables = `/sbin/iptables -L -n`;
@@ -14,13 +14,12 @@ sub ban_ip($)
     my ($ip) = @_;
 
     my $found = 0;
-    print "Protected IP: $prot_ip \n";
-    if ($ip =~ /$prot_ip/ && (length($prot_ip) > 0)) {
-            print "$ip is prjected \n";
+    if ($ip =~ /$prot_ip/ && (length($prot_ip) > 0) ) {
+            print "$ip is protected \n";
             return;
     }
     foreach my $line (@iptables) {
-        if ($line =~ /$ip/ && $line =~ /REJECT/ && $line =~ /dpt:22/) {
+        if ($line =~ /$ip/ && $line =~ /REJECT/ && $line =~ /dpt:25/) {
             $found = 1; last;
         }
     }
@@ -30,33 +29,36 @@ sub ban_ip($)
         return;
     }
    
-    my $cmd = "/sbin/iptables -A INPUT -s $ip -p tcp --destination-port 22 -j REJECT";
+    # my $cmd = "/sbin/iptables -A INPUT -s $ip -p tcp --destination-port 22 -j REJECT";
+    my $cmd = "/sbin/iptables -A INPUT -s $ip -p tcp --destination-port 25 -j REJECT";
     `$cmd`;
     print "+OK FAIL2BAN $ip\n";
 }
 
 MAIN: {
     # my @lines = `/usr/bin/tail -20000 /var/log/secure`;
-    my @lines = `cat /var/log/secure`;
+    my @lines = `cat /var/log/maillog`;
     my $intrusion = {};
     my $intrusion2 = {};
+    my $ban_total = 0;
     my $lent = length($prot_ip);
     print("Protected IP: $prot_ip  Len: $lent \n");
     foreach my $line (@lines) {
         # PAM 2 more authentication failures; logname= uid=0 euid=0 tty=ssh ruser= rhost=43.229.53.60  user=root
-        if ($line =~ /PAM\s+\d+\s+more authentication failures.+rhost=([\d\.]+).+user=(\S+)/) {
-            my ($ip, $uid) = ($1, $2);
-            if (! defined $intrusion->{$ip}) {
-                $intrusion->{$ip} = [];
-            }
-            push @{$intrusion->{$ip}}, $uid;
-        }
+    #    if ($line =~ /PAM\s+\d+\s+more authentication failures.+rhost=([\d\.]+).+user=(\S+)/) {
+    #        my ($ip, $uid) = ($1, $2);
+    #        if (! defined $intrusion->{$ip}) {
+    #            $intrusion->{$ip} = [];
+    #        }
+    #        push @{$intrusion->{$ip}}, $uid;
+    #    }
 
         # Invalid user support from 205.185.125.129 port 57184
         
         # Failed password for invalid user ubnt from 167.114.129.42 port 53685 ssh2
-        if ($line =~/Invalid user .+ from ([\d\.]+)/) {
+        if ($line =~/(\d+\.\d+\.\d+\.\d+).+did not issue MAIL\/EXPN\/VRFY\/ETRN during connection/){
             my $ip = $1;
+            # print "line: $line";
             if (! defined $intrusion2->{$ip}) {
                 $intrusion2->{$ip} = [];
             }
@@ -64,20 +66,19 @@ MAIN: {
         }
     }
 
-    foreach my $ip (keys %{$intrusion}) {
-        if (@{$intrusion->{$ip}} >= 1) {
-            # print "$ip: @{$intrusion->{$ip}}\n";
-            print "$ip is suspecious\n";
-            ban_ip($ip);
-        }
-    }
+    #foreach my $ip (keys %{$intrusion}) {
+    #    if (@{$intrusion->{$ip}} >= 1) {
+    #        # print "$ip: @{$intrusion->{$ip}}\n";
+    #        print "$ip is suspecious\n";
+    #        #ban_ip($ip);
+    #    }
+    #}
     
     foreach my $ip (keys %{$intrusion2}) {
         if (@{$intrusion2->{$ip}} >= 2) {
             print "$ip: @{$intrusion2->{$ip}}\n";
             print "$ip is suspecious\n";
-            ban_ip($ip);
+            ban_ip($ip)
         }
     }
-
 }
